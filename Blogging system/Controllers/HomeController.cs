3cfdaabbef78b3dad7d5e52bf277d1340fb324de
mohamed elkordy <mohamedelkordy127@gmail.com>
2019-06-05 23:03:43 +0200

    LastOne

--- Blogging system/Controllers/HomeController.cs
+++ Blogging system/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Blogging_system.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page";
 
             return View();
         }
